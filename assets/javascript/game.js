ea8f361a1e49f8a1ca7f0cc83e48edb0be59dd2a
MarcoTucsonpo <martucazpo@gmail.com>
2019-05-14 19:37:44 -0700

    more working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,6 @@
-var $ = function(id){
-    return document.getElementById(id);
-}
+
 var game = ["January", "February", "March", "April", "May"];
-var choice = [Math.floor(Math.random() * game.length)];
+var choice = Math.floor(Math.random() * 5);
 var answer = game[choice];
 var myLength = answer.length;
 var display = [myLength];
@@ -11,8 +9,6 @@ var letters = answer.split('');
 var attemptsLeft = 10;
 var output ="";
 var userLetter ="";
-
-
 
 var setup = function(){
     for ( i = 0; i < answer.length; i++)
@@ -23,21 +19,24 @@ var setup = function(){
     output = "";
 }
 
-window.onload = function{
+window.onload = function(){
     setup();
+    document.getElementsByClassName("guessLetter").onClick = submit();
 }
 
 var submit = function(){
 
+
     output = "";
-    userLetter=$("letter").value;
-    $("letter").value = "";
+    userLetter=document.getElementsByClassName("guessLetter").value;
+    document.getElementsByClassName("guessLetter").value = "";
 
     for (var c = 0; c < answer.length; c++)
     {
         if(userLetter.toUpperCase() == letters[c])
-        {display[c] = userLetter.toUpperCase();
-         win--;
+        {
+        display[c] = userLetter.toUpperCase();
+        win--;
         }
 
         output = output + display[c] + " ";
@@ -56,9 +55,4 @@ else if (attemptsLeft < 1){
 }
 else{
     document.getElementById("guesses").innerHTML = "You have " + attemptsLeft + " guesses left";
-}
-
-window.onload = function(){
-    setup();
-    $("submit").onclick = submit;
 }
